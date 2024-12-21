b32114bba9f945bc1b1f96a8720af63b2eda2fa2
sayeed <sayeed.705@gmail.com>
2024-12-21 21:44:16 +0530

    working version

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,40 @@
-import { Text, View } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+  useFrameProcessor,
+} from "react-native-vision-camera";
 
 export default function Index() {
+  const device = useCameraDevice("back");
+  const { hasPermission } = useCameraPermission();
+  const [showCamera, setShowCamera] = useState(false);
+
+  const requestPermission = async () => {
+    if (hasPermission) {
+      setShowCamera(true);
+    } else {
+      const permissionResult = await Camera.requestCameraPermission();
+      if (permissionResult) {
+        // Permission granted
+        setShowCamera(true);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission]);
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    "worklet";
+    console.log(`Frame: ${frame.width}x${frame.height} (${frame.pixelFormat})`);
+  }, []);
+
   return (
     <View
       style={{
@@ -9,6 +43,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      {device && hasPermission && showCamera && (
+        <Camera
+          style={StyleSheet.absoluteFill}
+          device={device}
+          isActive={true}
+          frameProcessor={frameProcessor}
+        />
+      )}
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
