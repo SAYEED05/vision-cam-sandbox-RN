a2a8cf91fb301b36d1e86769f55da50d1fb676f6
sayeed <sayeed.705@gmail.com>
2024-12-21 23:19:12 +0530

    wip

--- app/index.tsx
+++ app/index.tsx
@@ -1,41 +1,50 @@
-import { StyleSheet, Text, View } from "react-native";
-import { useEffect, useState, useRef } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  Dimensions,
+  Linking,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from "react-native";
 import {
   Camera,
-  runAsync,
+  CameraPosition,
   useCameraDevice,
-  useFrameProcessor,
+  useCameraFormat,
+  useCameraPermission,
   useSkiaFrameProcessor,
 } from "react-native-vision-camera";
 import {
-  Face,
-  useFaceDetector,
-  FaceDetectionOptions,
   Contours,
+  useFaceDetector,
 } from "react-native-vision-camera-face-detector";
-import { Worklets } from "react-native-worklets-core";
-import { Skia, TileMode, ClipOp } from "@shopify/react-native-skia";
+import { ClipOp, Skia, TileMode } from "@shopify/react-native-skia";
 
-export default function App() {
-  const faceDetectionOptions = useRef<FaceDetectionOptions>({
+function App(): React.JSX.Element {
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const [position, setPosition] = useState<CameraPosition>("front");
+  const device = useCameraDevice(position);
+  const format = useCameraFormat(device, [
+    {
+      videoResolution: Dimensions.get("window"),
+    },
+    {
+      fps: 60,
+    },
+  ]);
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission, requestPermission]);
+
+  const { detectFaces } = useFaceDetector({
     performanceMode: "fast",
     contourMode: "all",
     landmarkMode: "none",
     classificationMode: "none",
-  }).current;
-
-  const device = useCameraDevice("front");
-  const { detectFaces } = useFaceDetector(faceDetectionOptions);
-
-  useEffect(() => {
-    (async () => {
-      const status = await Camera.requestCameraPermission();
-      console.log({ status });
-    })();
-  }, [device]);
-
-  const handleDetectedFaces = Worklets.createRunOnJS((faces: Face[]) => {
-    console.log("faces detected", faces);
   });
 
   const blurRadius = 25;
@@ -48,17 +57,15 @@ export default function App() {
   const paint = Skia.Paint();
   paint.setImageFilter(blurFilter);
 
-  const frameProcessor = useSkiaFrameProcessor(
-    (frame) => {
-      "worklet";
-      // 1. Render frame as it is
-      frame.render();
+  const frameProcessor = useSkiaFrameProcessor((frame) => {
+    "worklet";
+    frame.render();
 
-      // 2. Detect faces in frame
-      const faces = detectFaces(frame);
+    const faces = detectFaces(frame);
 
-      // 3. Draw a blur mask over each face
-      for (const face of faces) {
+    for (const face of faces) {
+      if (face.contours != null) {
+        // this is a foreground face, draw precise mask with edges
         const path = Skia.Path.Make();
 
         const necessaryContours: (keyof Contours)[] = [
@@ -68,7 +75,7 @@ export default function App() {
         ];
         for (const key of necessaryContours) {
           const points = face.contours[key];
-          points.forEach((point: any, index: number) => {
+          points.forEach((point, index) => {
             if (index === 0) {
               // it's a starting point
               path.moveTo(point.x, point.y);
@@ -84,23 +91,87 @@ export default function App() {
         frame.clipPath(path, ClipOp.Intersect, true);
         frame.render(paint);
         frame.restore();
+      } else {
+        // this is a background face, just use a simple blur circle
+        const path = Skia.Path.Make();
+        console.log(`Face at ${face.bounds.x}, ${face.bounds.y}`);
+
+        const rect = Skia.XYWHRect(
+          face.bounds.x,
+          face.bounds.y,
+          face.bounds.width,
+          face.bounds.height
+        );
+        path.addOval(rect);
+
+        frame.save();
+        frame.clipPath(path, ClipOp.Intersect, true);
+        frame.render(paint);
+        frame.restore();
       }
-    },
-    [paint, detectFaces]
-  );
+    }
+  }, []);
+
+  const flipCamera = useCallback(() => {
+    setPosition((pos) => (pos === "front" ? "back" : "front"));
+  }, []);
 
   return (
-    <View style={{ flex: 1 }}>
-      {!!device ? (
-        <Camera
-          style={StyleSheet.absoluteFill}
-          device={device}
-          isActive={true}
-          frameProcessor={frameProcessor}
-        />
+    <View style={styles.container} onTouchEnd={flipCamera}>
+      {hasPermission ? (
+        device != null ? (
+          <Camera
+            style={styles.camera}
+            isActive={true}
+            device={device}
+            format={format}
+            frameProcessor={frameProcessor}
+            fps={format?.maxFps}
+            pixelFormat="rgb"
+            exposure={0}
+          />
+        ) : (
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>
+              Your phone does not have a {position} Camera.
+            </Text>
+          </View>
+        )
       ) : (
-        <Text>No Device</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.text} numberOfLines={5}>
+            This app needs Camera permission.{" "}
+            <Text style={styles.link} onPress={Linking.openSettings}>
+              Grant
+            </Text>
+          </Text>
+        </View>
       )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    maxWidth: "60%",
+    fontWeight: "bold",
+    fontSize: 15,
+    color: "black",
+  },
+  link: {
+    color: "rgb(80, 80, 255)",
+  },
+});
+
+export default App;
